91965c7a54f32b467efd7ddcd000c1ecaf441390
gogost6 <gogost6@gmail.com>
2026-02-23 13:18:08 +0200

    fix: brands desktop

--- components/brands.tsx
+++ components/brands.tsx
@@ -6,12 +6,14 @@ import { Zara } from "./icons/zara";
 
 export const Brands = () => {
   return (
-    <div className="flex flex-wrap gap-8 items-center justify-center px-4 py-10 bg-black gap-y-5">
-      <Versace />
-      <Zara />
-      <Gucci />
-      <Prada />
-      <CalvinKlein />
+    <div className="bg-black">
+      <div className="flex flex-wrap gap-8 mx-auto max-w-7xl items-center justify-center md:justify-between px-4 py-10 md:py-11 gap-y-5">
+        <Versace className="h-7 md:h-9" />
+        <Zara className="h-7 md:h-9" />
+        <Gucci className="h-7 md:h-9" />
+        <Prada className="h-7 md:h-9" />
+        <CalvinKlein className="h-7 md:h-9" />
+      </div>
     </div>
   );
 };
