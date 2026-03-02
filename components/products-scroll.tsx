40a5bb7d35ec9462584659d2213a078282204a85
gogost6 <gogost6@gmail.com>
2026-03-02 10:19:31 +0200

    fix: hardcoded href

--- components/products-scroll.tsx
+++ components/products-scroll.tsx
@@ -19,16 +19,19 @@ const ProductCard = ({
   price,
   discounted_price,
   percent_discount,
+  slug,
 }: ProductCardParams) => {
   return (
     <div className="flex-shrink-0 max-w-48 md:max-w-72">
-      <Image
-        src={url}
-        alt={alt || url.split(".")[0]}
-        width={298}
-        height={298}
-        className="mb-2.5 md:mb-4 w-48 h-48 md:w-72 md:h-72 rounded-2xl object-cover"
-      />
+      <Link href={`/shop/men/t-shirts/${slug}`}>
+        <Image
+          src={url}
+          alt={alt || url.split(".")[0]}
+          width={298}
+          height={298}
+          className="mb-2.5 md:mb-4 w-48 h-48 md:w-72 md:h-72 rounded-2xl object-cover hover:scale-[1.01] transition-transform"
+        />
+      </Link>
       <h3 className="font-bold text-base md:text-xl mb-1 md:mb-2">{title}</h3>
       <StarRating rating={rating} />
       {discounted_price ? (
@@ -86,6 +89,7 @@ export const ProductsScroll = async ({ title, type }: ProductsScrollParams) => {
               "https://tmfbysibhlpkahvvpoeu.supabase.co/storage/v1/object/public/shop.me/default-product.jpg"
             }
             alt={images?.find((img) => img.product_id === p.id)?.alt || p.title}
+            slug={p.slug}
           />
         ))}
       </div>
