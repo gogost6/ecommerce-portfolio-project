d322f1c80c1ecc8eac6b1d6257a5580512f57ef7
gogost6 <gogost6@gmail.com>
2026-03-03 13:42:27 +0200

    fix: product card comp

---
+++ components/product-card.tsx
@@ -0,0 +1,55 @@
+import { Database } from "@/database.types";
+import Image from "next/image";
+import Link from "next/link";
+import { StarRating } from "./star-rating";
+import { Badge } from "./ui/badge";
+
+type ProductCardParams = Database["public"]["Tables"]["products"]["Row"] & {
+  url: string;
+  alt?: string;
+  product_types: { slug: string };
+  categories: { slug: string };
+};
+
+export const ProductCard = ({
+  title,
+  rating,
+  url,
+  alt,
+  price,
+  discounted_price,
+  product_types,
+  categories,
+  slug,
+  gender,
+}: ProductCardParams) => {
+  const discountPercentage = discounted_price
+    ? Math.round(((price - discounted_price) / price) * 100)
+    : null;
+  return (
+    <div className="flex-shrink-0 max-w-48 md:max-w-72">
+      <Link
+        href={`/shop/${categories.slug}/${gender}/${product_types.slug}/${slug}`}
+      >
+        <Image
+          src={url}
+          alt={alt || url.split(".")[0]}
+          width={298}
+          height={298}
+          className="mb-2.5 md:mb-4 w-48 h-48 md:w-72 md:h-72 rounded-2xl object-cover hover:scale-[1.01] transition-transform"
+        />
+      </Link>
+      <h3 className="font-bold text-base md:text-xl mb-1 md:mb-2">{title}</h3>
+      <StarRating rating={rating} />
+      {discounted_price ? (
+        <div className="font-bold text-xl md:text-2xl mt-1 md:mt-2 flex items-center justify-start gap-2.5">
+          <span>${discounted_price}</span>
+          <span className="line-through text-gray-500">${price}</span>{" "}
+          <Badge variant={"red"}>-{discountPercentage}%</Badge>
+        </div>
+      ) : (
+        <p className="font-bold text-xl md:text-2xl mt-1 md:mt-2">${price}</p>
+      )}
+    </div>
+  );
+};
