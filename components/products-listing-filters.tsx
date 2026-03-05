95190ff68911edb0f8abd4c03d983e4488f65905
gogost6 <gogost6@gmail.com>
2026-03-05 21:32:46 +0200

    fix: scroll and close upon clicking outside

--- components/products-listing-filters.tsx
+++ components/products-listing-filters.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 import { useFiltersOpenerStore } from "@/store/filters-opener-store";
 import * as Slider from "@radix-ui/react-slider";
 import { ChevronDown, ChevronRight, X } from "lucide-react";
+import { useEffect } from "react";
 import { Button } from "./ui/button";
 
 type ProductsListingFiltersProps = {
@@ -21,86 +22,111 @@ export function ProductsListingFilters({
 }: ProductsListingFiltersProps) {
   const { isOpen, setIsOpen } = useFiltersOpenerStore();
 
+  useEffect(() => {
+    const isMobile = window.innerWidth < 768;
+
+    if (!isMobile) return;
+
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [isOpen]);
+
   return (
-    <div
-      className={cn(
-        "p-5 bg-white rounded-t-2xl absolute z-20 w-full left-0 transform translate-y-full shadow-lg",
-        { "translate-y-0": isOpen },
-      )}
-    >
-      <div className="flex justify-between items-center mb-4">
-        <h2 className="font-bold text-xl">Filters</h2>
-        <X
-          size={24}
+    <>
+      {isOpen && (
+        <div
+          className="fixed inset-0 bg-black/40 z-10 md:hidden"
           onClick={() => setIsOpen(false)}
-          className="cursor-pointer"
         />
-      </div>
-      <div className="h-[1px] bg-gray-100 mb-5"></div>
-      <div className="flex flex-col gap-5 justify-between items-center mb-6">
-        {productTypes?.map((type) => (
-          <div
-            key={type.id}
-            className="flex items-center justify-between w-full"
-          >
-            <p className="text-base text-gray-600">{type.name}</p>
-            <ChevronRight size={16} />
-          </div>
-        ))}
-      </div>
-      <div className="h-[1px] bg-gray-100 mb-5"></div>
-      <div className="flex items-center justify-between mb-4">
-        <h2 className="font-bold text-xl">Price</h2>
-        <ChevronDown size={16} />
-      </div>
-      <Slider.Root className="mb-5">
-        <Slider.Track>
-          <Slider.Range />
-        </Slider.Track>
-        <Slider.Thumb />
-      </Slider.Root>
-      <div className="h-[1px] bg-gray-100 mb-5"></div>
-      <div className="flex items-center justify-between mb-4">
-        <h2 className="font-bold text-xl">Colors</h2>
-        <ChevronDown size={16} />
-      </div>
-      <div className="flex gap-4 flex-wrap mb-6">
-        {colors?.map((color) => (
-          <div
-            key={color.id}
-            className="w-8 h-8 rounded-full ring-1 ring-gray-200 ring-offset-1 cursor-pointer"
-            style={{ backgroundColor: color.hex }}
+      )}
+
+      <div
+        className={cn(
+          "p-5 pb-8 bg-white rounded-t-2xl absolute z-20 w-full left-0 transform translate-y-full shadow-lg max-h-[85vh] overflow-y-auto",
+          { "-translate-y-16": isOpen },
+        )}
+      >
+        <div className="flex justify-between items-center mb-4">
+          <h2 className="font-bold text-xl">Filters</h2>
+          <X
+            size={24}
+            onClick={() => setIsOpen(false)}
+            className="cursor-pointer"
           />
-        ))}
+        </div>
+        <div className="h-[1px] bg-gray-100 mb-5"></div>
+        <div className="flex flex-col gap-5 justify-between items-center mb-6">
+          {productTypes?.map((type) => (
+            <div
+              key={type.id}
+              className="flex items-center justify-between w-full"
+            >
+              <p className="text-base text-gray-600">{type.name}</p>
+              <ChevronRight size={16} />
+            </div>
+          ))}
+        </div>
+        <div className="h-[1px] bg-gray-100 mb-5"></div>
+        <div className="flex items-center justify-between mb-4">
+          <h2 className="font-bold text-xl">Price</h2>
+          <ChevronDown size={16} />
+        </div>
+        <Slider.Root className="mb-5">
+          <Slider.Track>
+            <Slider.Range />
+          </Slider.Track>
+          <Slider.Thumb />
+        </Slider.Root>
+        <div className="h-[1px] bg-gray-100 mb-5"></div>
+        <div className="flex items-center justify-between mb-4">
+          <h2 className="font-bold text-xl">Colors</h2>
+          <ChevronDown size={16} />
+        </div>
+        <div className="flex gap-4 flex-wrap mb-6">
+          {colors?.map((color) => (
+            <div
+              key={color.id}
+              className="w-8 h-8 rounded-full ring-1 ring-gray-200 ring-offset-1 cursor-pointer"
+              style={{ backgroundColor: color.hex }}
+            />
+          ))}
+        </div>
+        <div className="h-[1px] bg-gray-100 mb-5"></div>
+        <div className="flex items-center justify-between mb-4">
+          <h2 className="font-bold text-xl">Size</h2>
+          <ChevronDown size={16} />
+        </div>
+        <div className="flex gap-2 flex-wrap mb-5">
+          {sizes?.map((size) => (
+            <Button key={size.id} variant={"secondary"} size={"sm"}>
+              {size.name}
+            </Button>
+          ))}
+        </div>
+        <div className="h-[1px] bg-gray-100 mb-5"></div>
+        <div className="flex items-center justify-between mb-4">
+          <h2 className="font-bold text-xl">Dress Style</h2>
+          <ChevronDown size={16} />
+        </div>
+        <div className="flex flex-col gap-5 justify-between items-center mb-6">
+          {categories?.map((cat) => (
+            <div
+              key={cat.id}
+              className="flex items-center justify-between w-full"
+            >
+              <p className="text-base text-gray-600">{cat.name}</p>
+              <ChevronRight size={16} />
+            </div>
+          ))}
+        </div>
       </div>
-      <div className="h-[1px] bg-gray-100 mb-5"></div>
-      <div className="flex items-center justify-between mb-4">
-        <h2 className="font-bold text-xl">Size</h2>
-        <ChevronDown size={16} />
-      </div>
-      <div className="flex gap-2 flex-wrap mb-5">
-        {sizes?.map((size) => (
-          <Button key={size.id} variant={"secondary"} size={"sm"}>
-            {size.name}
-          </Button>
-        ))}
-      </div>
-      <div className="h-[1px] bg-gray-100 mb-5"></div>
-      <div className="flex items-center justify-between mb-4">
-        <h2 className="font-bold text-xl">Dress Style</h2>
-        <ChevronDown size={16} />
-      </div>
-      <div className="flex flex-col gap-5 justify-between items-center mb-6">
-        {categories?.map((cat) => (
-          <div
-            key={cat.id}
-            className="flex items-center justify-between w-full"
-          >
-            <p className="text-base text-gray-600">{cat.name}</p>
-            <ChevronRight size={16} />
-          </div>
-        ))}
-      </div>
-    </div>
+    </>
   );
 }
