ce18036c28ee445eed9d482ad64de51071dd34c6
gogost6 <gogost6@gmail.com>
2026-03-15 16:37:53 +0200

    fix: enable drizzle query

--- db/index.ts
+++ db/index.ts
@@ -1,9 +1,16 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+import * as relations from "../drizzle/relations";
+import * as schema from "../drizzle/schema";
 
 const client = postgres(process.env.DATABASE_URL!, {
   prepare: false,
 });
 
-export const db = drizzle(client);
+export const db = drizzle(client, {
+  schema: {
+    ...schema,
+    ...relations,
+  },
+});
